916d27a6d4c8ae1dbd98cea301a75c97daaf2b50
Jeniffer Klein <56811604+JenifferKlein@users.noreply.github.com>
2023-08-17 02:38:44 +0000

    refinamento do código e arquivo de texto adicionado

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,90 +1,98 @@
 class CaixaDaLanchonete {
+    //lista com os valores dos itens
+    precos = {
+        cafe: {
+            price: 3.00,
+        },
+        chantily: {
+            price: 1.50,
+            requires: 'cafe',
+        },
+        suco: {
+            price: 6.20,
+        },
+        sanduiche: {
+            price: 6.50,
+        },
+        queijo: {
+            price: 2.00,
+            requires: 'sanduiche',
+        },
+        salgado: {
+            price: 7.25,
+        },
+        combo1: {
+            price: 9.50,
+        },
+        combo2: {
+            price: 7.50,
+        },
+    };
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-        //lista de precos
-        const precos = {
-            cafe: 3.00,
-            chantily: 1.50, //item extra(cafe)
-            suco: 6.20,
-            sanduiche: 6.50,
-            queijo: 2.00, //item extra(sanduiche)
-            salgado: 7.25,
-            combo1: 9.50,
-            combo2: 7.50
-        };
         //variavel para armazenar o total da compra
-        let total = 0; 
-        //metodo rensponsavel para verificar se um item extra foi pedido sem o item principal
-        const verifica = this.verificaItem(itens); 
-        if(verifica === 1){                       
+        let total = 0;
+        //metodo responsavel para verificar se algum item extra foi pedido sem ter o acompanhamento de um item principal
+        const verifica = this.verificaItem(itens);
+        if (!verifica) {
             return "Item extra não pode ser pedido sem o principal";
         }
         //verifica se um carrinho esta vazio
-        if(itens.length === 0){
+        if (itens.length === 0) {
             return "Não há itens no carrinho de compra!";
         }
         //para cada item da lista, verifica se a quantidade é valida e se o item existe na lista
         for (const item of itens) {
             const [nome, quantidade] = item.split(',');
-            if(quantidade <= 0){
+            if (quantidade <= 0) {
                 return "Quantidade inválida!";
             }
-            if(!precos[nome]){
+            if (!this.precos[nome]) {
                 return "Item inválido!";
             }
-            //se ele existir então é calculado o preco total
-            if (precos[nome]) {
-                total += precos[nome] * parseInt(quantidade);
-            }
+            //se o item existir então é feito o calculo do preco total
+            total += this.precos[nome].price * parseInt(quantidade);
         }
         //verifica a forma de pagamento para poder fazer o calculo de acordo com o soliciado para cada metodo de pagamento
-        const formaPagamento = metodoDePagamento;
-        switch(formaPagamento){
+        switch (metodoDePagamento) {
             case 'credito':
                 total += (total * 0.03);
-                return this.formataValor(total.toFixed(2));
+                return this.formataValor(total);
             case 'debito':
-                return this.formataValor(total.toFixed(2));
+                return this.formataValor(total);
             case 'dinheiro':
                 total *= 0.95;
-                return this.formataValor(total.toFixed(2));
+                return this.formataValor(total);
             default:
                 return "Forma de pagamento inválida!";
         }
     }
     //metodo para formatar o valor adicionando o R$ e trocando o . por ,
     formataValor(valor) {
-        //return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        return `R$ ${valor.toString().replace(".", ",")}`;
+        //Este return comentado é uma outra alternativa para a formatação do valor
+        // return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        return `R$ ${valor.toFixed(2).replace(".", ",")}`;
     }
-
-    verificaItem(itens){
-        let cont1 = 0;
-        let cont2 = 0;
-        let cont3 = 0;
-        let cont4 = 0;
-        for(const item of itens){
+    // valida se o item extra foi pedido com pelo menos um item principal
+    verificaItem(itens) {
+        for (const item of itens) {
             const [nome, quantidade] = item.split(',');
-            if(nome === 'chantily' && quantidade > 0){
-                cont1++;
-            }
-            if(nome === 'cafe' && quantidade > 0){
-                cont2++;
-            }
-            if(nome === 'queijo' && quantidade > 0){
-                cont3++;
-            }
-            if(nome === 'sanduiche' && quantidade > 0){
-                cont4++;
+            if (quantidade > 0 && this.precos[nome]?.requires) {
+                let hasAnyItem = false;
+                const necessita = this.precos[nome].requires
+                for (const item2 of itens) {
+                    const [nome2, quantidade2] = item2.split(',');
+                    if (nome2 === necessita && quantidade2 > 0) {
+                        hasAnyItem = true;
+                        break;
+                    }
+                }
+                if (!hasAnyItem) {
+                    return false;
+                }
             }
         }
-        if(cont1 > 0  && cont2 === 0){
-            return 1;
-        }
-        if(cont3 > 0  && cont4 === 0){
-            return 1;
-        }
+        return true;
     }
 }
-
 export { CaixaDaLanchonete };
